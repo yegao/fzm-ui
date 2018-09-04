617d8077b83152ba872c11ce5ffee72b99a1da3b
yegao <familyink@163.com>
2018-09-04 15:20:11 +0800

    钩子

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Logreg from './packages/logreg/index.js';
+const components = [Logreg];
+
+let install = function(Vue) {
+  components.map(component => {
+    Vue.component(component.name, component);
+  });
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export default {version: '1.2.5', install}
+
+// module.exports = {
+//   version: '1.0.1',
+//   install
+// };
+//
+// module.exports.default = module.exports;
